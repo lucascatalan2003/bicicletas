e3ad39721ddf38086fbd2ee1a84954b095a3b440
Lucas Catalan <ramirocatalan03@gmail.com>
2024-04-04 14:48:54 -0300

    Trabajo Terminado

---
+++ js/bicicletas.js
@@ -0,0 +1,33 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const bicicletas = [
+  {
+    id:1,
+    nombre: "Veloziraptor",
+    precio: 1250500,
+  },
+  {
+    id:2,
+    nombre: "Biciclón",
+    precio: 2300000,
+  },
+  {
+    id:3,
+    nombre: "CicloTravesura",
+    precio: 2800000,
+  },
+  {
+    id:4,
+    nombre: "Pedalástica",
+    precio: 1700000,
+  },
+  {
+    id:5,
+    nombre: "RuedaFrenesí",
+    precio: 1200000,
+  },
+  {
+    id:6,
+    nombre: "CicloLoco",
+    precio: 2400000,
+  }
+]
